02b0d01f29bf16d1675a33da37364b6d1a2858c1
Ankit Kumar <nktkumar82@gmail.com>
2019-04-12 22:30:39 +0530

    Normal binding

--- app/src/main/java/com/captain/ak/data_binding/MainActivity.java
+++ app/src/main/java/com/captain/ak/data_binding/MainActivity.java
@@ -1,13 +1,21 @@
 package com.captain.ak.data_binding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.captain.ak.data_binding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        Contact contact = new Contact("Ankit" , "Kumar");
+        activityMainBinding.setContact(contact);
+
     }
 }
