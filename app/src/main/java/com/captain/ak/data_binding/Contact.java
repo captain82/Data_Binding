bbd20ce26b3d61ce05f605a3830034cbc2ebd2ee
Ankit Kumar <nktkumar82@gmail.com>
2019-04-12 21:10:08 +0530

    Data binding enabled and Data class is build

---
+++ app/src/main/java/com/captain/ak/data_binding/Contact.java
@@ -0,0 +1,13 @@
+package com.captain.ak.data_binding;
+
+public class Contact {
+
+    public final String firstName,lastName;
+
+
+    public Contact(String firstName, String lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
